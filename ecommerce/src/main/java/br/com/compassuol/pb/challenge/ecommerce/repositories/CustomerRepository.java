f84b6722153db00a664ea244a51d386cd02938f3
Ivan Pinheiro <Ivanppf1@gmail.com>
2023-06-06 16:01:18 -0300

    adicionando customer simples com lombok e data rest

---
+++ ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/repositories/CustomerRepository.java
@@ -0,0 +1,7 @@
+package br.com.compassuol.pb.challenge.ecommerce.repositories;
+
+import br.com.compassuol.pb.challenge.ecommerce.entities.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CustomerRepository extends JpaRepository<Customer, Integer> {
+}
