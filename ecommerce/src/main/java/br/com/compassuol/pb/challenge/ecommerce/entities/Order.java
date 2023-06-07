a34d8886b1c481b2c0a23a4b72e33c8afd3d5ca9
Rharhuky <rharhuky@gmail.com>
2023-06-07 16:50:00 -0300

    Adicionando OrderRestController

--- ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/entities/Order.java
+++ ecommerce/src/main/java/br/com/compassuol/pb/challenge/ecommerce/entities/Order.java
@@ -2,11 +2,19 @@ package br.com.compassuol.pb.challenge.ecommerce.entities;
 
 import br.com.compassuol.pb.challenge.ecommerce.enums.StatusOption;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "orders")
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
 public class Order {
 
     @Id
@@ -23,5 +31,10 @@ public class Order {
     @Column(name = "status")
     private StatusOption status;
 
-
+    public Order(int orderId, int customerId, LocalDate orderDate, StatusOption status) {
+        this.orderId = orderId;
+        this.customerId = customerId;
+        this.orderDate = orderDate;
+        this.status = status;
+    }
 }
