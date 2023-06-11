5bf6c198f347a08b93861c4326296e7986cafb58
GabrielHR0sa <rosahenriquegabriel@gmail.com>
2023-06-11 14:15:44 -0300

    adicionando os testes do customerRestController

--- ecommerce/src/test/java/br/com/compassuol/pb/challenge/ecommerce/controllers/CustomerRestControllerTest.java
+++ ecommerce/src/test/java/br/com/compassuol/pb/challenge/ecommerce/controllers/CustomerRestControllerTest.java
@@ -1,27 +1,60 @@
 package br.com.compassuol.pb.challenge.ecommerce.controllers;
 
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
 
+import br.com.compassuol.pb.challenge.ecommerce.entities.Customer;
+import br.com.compassuol.pb.challenge.ecommerce.services.CustomerServiceImpl;
+
+@ExtendWith(MockitoExtension.class)
 public class CustomerRestControllerTest {
 
+    @InjectMocks
+    private CustomerRestController customerRestController;
+
+    @Mock
+    private CustomerServiceImpl customerServiceImpl;
+    private ResponseEntity<Customer> respostaCustomer;
+    
+    private Customer customerProps;
+
     @BeforeEach
-    void setup() {
-
-    }
-
-    @Test
-    public void testGetCustomer() throws Exception {
-
+    void setup(){
+        customerProps = new Customer("gabriel","12345678910","gabriel@gmail.com",true);
+        respostaCustomer = customerServiceImpl.getCustomerById(1);
     }
 
     @Test
     void testAddCustomer() {
+        var response = assertDoesNotThrow(() -> customerRestController.addCustomer(customerProps));
+        assertEquals(customerServiceImpl.saveCustomer(customerProps), response);
+    }
+
+    @Test
+    void testGetCustomer() {
+       
+        when(customerServiceImpl.getCustomerById(1)).thenReturn(respostaCustomer);
+        var response = assertDoesNotThrow(() -> customerRestController.getCustomerById(1));
+        assertEquals(respostaCustomer,response);
     }
 
     @Test
     void testUpdateCustomer() {
-
+        var response = assertDoesNotThrow(() -> customerRestController.updateCustomer(1,customerProps));
+        assertEquals(customerServiceImpl.updateCustomer(1,customerProps), response);
     }
 
 }
