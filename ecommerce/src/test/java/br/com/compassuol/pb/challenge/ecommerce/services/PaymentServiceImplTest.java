4a690ee1c107e837043c066c0aa81d5a20a6aa97
Luigi Belanda <luigi.belanda.milani@gmail.com>
2023-06-12 17:55:59 -0300

    Testes Order

--- ecommerce/src/test/java/br/com/compassuol/pb/challenge/ecommerce/services/PaymentServiceImplTest.java
+++ ecommerce/src/test/java/br/com/compassuol/pb/challenge/ecommerce/services/PaymentServiceImplTest.java
@@ -1,27 +1,12 @@
 package br.com.compassuol.pb.challenge.ecommerce.services;
 
-import br.com.compassuol.pb.challenge.ecommerce.dto.OrderDTO;
-import br.com.compassuol.pb.challenge.ecommerce.dto.PaymentDTO;
-import br.com.compassuol.pb.challenge.ecommerce.entities.Order;
-import br.com.compassuol.pb.challenge.ecommerce.entities.Payment;
-import br.com.compassuol.pb.challenge.ecommerce.enums.StatusOptions;
 import br.com.compassuol.pb.challenge.ecommerce.repositories.OrderRepository;
 import br.com.compassuol.pb.challenge.ecommerce.repositories.PaymentRepository;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import java.time.LocalDate;
-import java.util.Optional;
-
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class PaymentServiceImplTest {
